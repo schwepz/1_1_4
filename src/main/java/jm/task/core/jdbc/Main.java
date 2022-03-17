eb68ce28c978e165a8a1a1e89415a81ed930eedf
Administrator <kirillschwepz@gmail.com>
2022-03-17 17:17:57 +0700

    commit 17032022

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,26 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
+
+
 public class Main {
+    private final static UserService userService = new UserServiceImpl();
+
     public static void main(String[] args) {
-        // реализуйте алгоритм здесь
+        userService.createUsersTable();
+
+        userService.saveUser("Кирилл", "Курскеев", (byte) 25);
+        userService.saveUser("Не Кирилл", "Борисович", (byte) 10);
+        userService.saveUser("Еще ОДин ", "Человек", (byte) 55);
+        userService.saveUser("Тейп", "на бабках", (byte) 20);
+
+        userService.removeUserById(2);
+
+        userService.getAllUsers();
+
+        userService.cleanUsersTable();
+
+        userService.dropUsersTable();
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,38 @@
 package jm.task.core.jdbc.util;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    private static Connection conn = null;
+    private static Util instance = null;
+
+    private Util() {
+        try {
+            if (null == conn || conn.isClosed()) {
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/mysql", "root", "Ah1fl8NqN%muiip") ;
+            }
+        } catch (SQLException  e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static Util getInstance() {
+        if (null == instance) {
+            instance = new Util();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() {
+        return conn;
+    }
+
 }
